632263eabe2686bd18926fde72b8fd33e001e142
thomasJur <pyristfeuer@googlemail.com>
2019-01-26 13:07:08 +0100

    Now added the above mentioned fixes

--- scraper.py
+++ scraper.py
@@ -4,22 +4,24 @@ import datetime
 
 def scrapeHeadlinesLong():
     response = requests.get("http://www.spiegel.de")
-    content = reponse.content
+    content = response.content
     scraper = BeautifulSoup(content, "html.parser")
     headlines_long = scraper.select(".headline")
     headlines_long_list = []
     for entry in headlines_long:
-        headlines_long_list.append(entry)
+        if entry.text != "":
+            headlines_long_list.append(entry.text)
     return (headlines_long_list, getTime())
 
 def scrapeHeadlinesShort():
     response = requests.get("http://www.spiegel.de")
-    content = reponse.content
+    content = response.content
     scraper = BeautifulSoup(content, "html.parser")
     headlines_short = scraper.select(".headline")
     headlines_short_list = []
     for entry in headlines_short:
-        headlines_short_list.append(entry)
+        if entry.text != "":
+            headlines_short_list.append(entry.text)
     return (headlines_short_list, getTime())
 
 def getTime():
