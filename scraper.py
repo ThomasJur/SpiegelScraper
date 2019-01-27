4992bfb5dbaa52b546acac3b836cd4636e907910
thomasJur <pyristfeuer@googlemail.com>
2019-01-27 22:35:23 +0100

    added functions to store the scraped data in a database "db/spon.db"

--- scraper.py
+++ scraper.py
@@ -1,16 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 import datetime
+import sqlite3
+from sqlite3 import Error
+
+# statements for sql inserts/queries
+sql_insert = "INSERT INTO spon (headline, link, article_short, date) VALUES(?,?,?,?)"
 
 def scrapeArticleSPON():
     response = requests.get("https://www.spiegel.de")
     scraper = BeautifulSoup(response.content, "html.parser")
     query = scraper.select(".teaser")
+    #esatblishing database connection
+    conn = connect_db()
     for ind, element in enumerate(query):
         if element.text is not None:
             headline, text, link = getFeaturesSPON(element)
+            c = conn.cursor()
             if not ((headline=="no") or (text == "no") or (link == "no")):
                 print("Article {} \n".format(ind+1) + "Headline: " + headline + "\n" + "Link: " + link + "\n" + "Text: " + text + "\n")
+                if rowExists(conn, headline):
+                    c.execute(sql_insert,(headline, link, text, str(getTime())))
+    conn.commit()
+    conn.close()
 
 def getTime():
     now = datetime.datetime.now()
@@ -33,3 +45,22 @@ def getFeaturesSPON(in_query):
     else:
         text = "no"
     return (headline, text, link)
+
+def connect_db():
+    try:
+        conn = sqlite3.connect("db/spon.db")
+    except Error as e:
+        print(e)
+    return conn
+
+def rowExists(conn, headline):
+    c = conn.cursor()
+    row = c.execute("SELECT * FROM spon WHERE (headline=?)", (headline,)).fetchone()
+    if row is None:
+        return True
+    else:
+        return False
+
+
+if __name__=="__main__":
+    scrapeArticleSPON()
