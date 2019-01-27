d3ee444af3860a2cfa752c5a39cb7725c03bfe38
thomasJur <pyristfeuer@googlemail.com>
2019-01-27 14:37:09 +0100

    branch commit scrape spon articles

--- scraper.py
+++ scraper.py
@@ -2,28 +2,34 @@ import requests
 from bs4 import BeautifulSoup
 import datetime
 
-def scrapeHeadlinesLong():
-    response = requests.get("http://www.spiegel.de")
-    content = response.content
-    scraper = BeautifulSoup(content, "html.parser")
-    headlines_long = scraper.select(".headline")
-    headlines_long_list = []
-    for entry in headlines_long:
-        if entry.text != "":
-            headlines_long_list.append(entry.text)
-    return (headlines_long_list, getTime())
-
-def scrapeHeadlinesShort():
-    response = requests.get("http://www.spiegel.de")
-    content = response.content
-    scraper = BeautifulSoup(content, "html.parser")
-    headlines_short = scraper.select(".headline")
-    headlines_short_list = []
-    for entry in headlines_short:
-        if entry.text != "":
-            headlines_short_list.append(entry.text)
-    return (headlines_short_list, getTime())
+def scrapeArticleSPON():
+    response = requests.get("https://www.spiegel.de")
+    scraper = BeautifulSoup(response.content, "html.parser")
+    query = scraper.select(".teaser")
+    for ind, element in enumerate(query):
+        if element.text is not None:
+            headline, text, link = getFeaturesSPON(element)
+            if not ((headline=="no") or (text == "no") or (link == "no")):
+                print("Article {} \n".format(ind+1) + "Headline: " + headline + "\n" + "Link: " + link + "\n" + "Text: " + text + "\n")
 
 def getTime():
     now = datetime.datetime.now()
     return now.isoformat()
+
+def getFeaturesSPON(in_query):
+    if in_query.find("a"):
+        if in_query.a.has_attr("title"):
+            headline = in_query.a["title"]
+        else:
+            headline="no"
+    else:
+        headline = "no"
+    if in_query.find("a"):    
+        link = "http://www.spiegel.de" + in_query.a["href"]
+    else:
+        link = "no"
+    if in_query.find("p"):
+        text = in_query.p.text.strip()
+    else:
+        text = "no"
+    return (headline, text, link)
